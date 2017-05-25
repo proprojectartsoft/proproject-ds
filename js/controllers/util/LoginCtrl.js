c7b7591f319545235d189cd30898a60021703c5c
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-05-25 12:25:33 +0300

    offline automatically login

--- js/controllers/util/LoginCtrl.js
+++ js/controllers/util/LoginCtrl.js
@@ -31,15 +31,11 @@ angular.module($APP.name).controller('LoginCtrl', [
                     }
                 }).error(function(response, status) {
                     if (status === 0 || status === -1) {
-                        if (localStorage.getObject('automLogin'))
-                            SyncService.sync();
-                        else {
-                            var alertPopup = $ionicPopup.alert({
-                                title: 'Offline',
-                                template: "<center>You are offline. Please check your internet connection and try again.</center>",
-                            });
-                            alertPopup.then(function(res) {});
-                        }
+                        var alertPopup = $ionicPopup.alert({
+                            title: 'Offline',
+                            template: "<center>You are offline. Please check your internet connection and try again.</center>",
+                        });
+                        alertPopup.then(function(res) {});
                     }
                     if (status === 502) {
                         var alertPopup = $ionicPopup.alert({
@@ -71,7 +67,34 @@ angular.module($APP.name).controller('LoginCtrl', [
             $scope.user.password = localStorage.getObject('dsremember').password;
             $scope.user.remember = localStorage.getObject('dsremember').remember;
             if (localStorage.getObject('automLogin'))
-                $scope.login();
+                AuthService.login($scope.user).success(function(result) {
+                    SyncService.sync();
+                }).error(function(response, status) {
+                    if (status === 0 || status === -1) {
+                        $state.go('app.projects');
+                    }
+                    if (status === 502) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: 'Offline',
+                            template: "<center>Server offline</center>",
+                        });
+                        alertPopup.then(function(res) {});
+                    }
+                    if (status === 400) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: 'Error',
+                            template: "<center>Incorrect user data</center>",
+                        });
+                        alertPopup.then(function(res) {});
+                    }
+                    if (status === 401) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: 'Error',
+                            template: 'Your account has been de-activated. Contact your supervisor for further information',
+                        });
+                        alertPopup.then(function(res) {});
+                    }
+                })
         }
     }
 ]);
